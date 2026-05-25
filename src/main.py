42a19be4ba6322d26dabe28a6c1f3d063076e2ee
b-zago <bart.zagozdon@gmail.com>
2026-05-25 21:49:30 +0200

    code refactor for lambda again

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ import os
 
 from pydantic import BaseModel
 from datetime import datetime, timezone
-from dataclasses import dataclass
 
 
 class Subject(BaseModel):
@@ -22,28 +21,28 @@ class Payload(BaseModel):
     message: str
 
 
-TIMEOUT = int(os.environ["TIMEOUT"])  # get from env/config or hardcode
-HMAC_KEY = os.environ["HMAC_KEY"]
-
 WEBHOOK_URL = "https://nyanify.zagoapps.com/webhook"
 SIG_HEADER = "x-nyanify-signature"
-RECEIVER_ID = os.environ["RECEIVER_ID"]
-
-dynamodb = boto3.resource("dynamodb")
-ssm = boto3.client("ssm")
-
-table = dynamodb.Table(os.environ["NYANWATCH_TABLE"])
-
-failures = table.scan()["Items"]
-
-failed_services = [service["service"] for service in failures]
-
-now = datetime.now(timezone.utc)
-
-print(failed_services)
 
 
 async def main():
+    TIMEOUT = int(os.environ["TIMEOUT"])  # get from env/config or hardcode
+    HMAC_KEY = os.environ["HMAC_KEY"]
+    RECEIVER_ID = int(os.environ["RECEIVER_ID"])
+
+    dynamodb = boto3.resource("dynamodb")
+    ssm = boto3.client("ssm")
+
+    table = dynamodb.Table(os.environ["NYANWATCH_TABLE"])
+
+    failures = table.scan()["Items"]
+
+    failed_services = [service["service"] for service in failures]
+
+    now = datetime.now(timezone.utc)
+
+    print(failed_services)
+
     parameter = ssm.get_parameter(Name="/nyanwatch/endpoints")
     raw_value = parameter.get("Parameter").get("Value")
 
@@ -66,20 +65,43 @@ async def main():
                 except httpx.TimeoutException as e:
                     if wasHealthy:
                         new_failures.append(
-                            await new_failure(subject, "TIMEOUT", client, svc_err=e)
+                            await new_failure(
+                                subject,
+                                "TIMEOUT",
+                                client,
+                                now,
+                                RECEIVER_ID,
+                                HMAC_KEY,
+                                svc_err=e,
+                            )
                         )
                     return
                 except httpx.HTTPError as e:
                     if wasHealthy:
                         new_failures.append(
-                            await new_failure(subject, "DEAD", client, svc_err=e)
+                            await new_failure(
+                                subject,
+                                "DEAD",
+                                client,
+                                now,
+                                RECEIVER_ID,
+                                HMAC_KEY,
+                                svc_err=e,
+                            )
                         )
                     return
 
                 if (r.status_code != 200) and wasHealthy:
                     new_failures.append(
                         await new_failure(
-                            subject, "NOT_OK", client, r.status_code, r.text
+                            subject,
+                            "NOT_OK",
+                            client,
+                            now,
+                            RECEIVER_ID,
+                            HMAC_KEY,
+                            r.status_code,
+                            r.text,
                         )
                     )
                     return
@@ -94,25 +116,32 @@ async def main():
                     await webhook_send(
                         f"nyaaaa~~\n\n{subject.service} works now! it was down for about {down_duration}\n\nGood job! ^^\n\n{subject}",
                         client,
+                        RECEIVER_ID,
+                        HMAC_KEY,
                     )
 
                     new_healthy.append(subject.service)
 
         await asyncio.gather(*(fetch(subject) for subject in value))
-        batch_write(new_failures)
-        batch_delete(new_healthy)
+        batch_write(new_failures, table)
+        batch_delete(new_healthy, table)
 
 
 async def new_failure(
     subject: Subject,
     failure_type: str,
     client: httpx.AsyncClient,
+    now,
+    RECEIVER_ID,
+    HMAC_KEY,
     status=0,
     svc_err="None",
 ):
     await webhook_send(
         f"OOOPSIEEE\n\n{subject.service} had unexpected response of {status}!\nIt is officially {failure_type} - fixx nowww!!!!\n\n{subject}\n\nERROR: {svc_err}",
         client,
+        RECEIVER_ID,
+        HMAC_KEY,
     )
     return {
         "service": subject.service,
@@ -122,19 +151,19 @@ async def new_failure(
     }
 
 
-def batch_write(new_failures: list):
+def batch_write(new_failures: list, table):
     with table.batch_writer() as batch:
         for failure in new_failures:
             batch.put_item(failure)
 
 
-def batch_delete(new_healthy: list):
+def batch_delete(new_healthy: list, table):
     with table.batch_writer() as batch:
         for healthy in new_healthy:
             batch.delete_item(Key={"service": healthy})
 
 
-async def webhook_send(message: str, client: httpx.AsyncClient):
+async def webhook_send(message: str, client: httpx.AsyncClient, RECEIVER_ID, HMAC_KEY):
     payload = Payload(receiver_id=RECEIVER_ID, message=message)
     sig = hmac.new(
         HMAC_KEY.encode(), payload.model_dump_json().encode(), hashlib.sha256
